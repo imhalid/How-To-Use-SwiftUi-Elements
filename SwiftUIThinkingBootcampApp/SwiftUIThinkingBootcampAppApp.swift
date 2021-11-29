eb7bc7bec5989a4537d821d8dcedf6fd1a978e9e
imhalid <imhalid@icloud.com>
2021-11-29 16:36:36 +0300

    Initial Commit

---
+++ SwiftUIThinkingBootcampApp/SwiftUIThinkingBootcampAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingBootcampAppApp.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by Halid İçli on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingBootcampAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

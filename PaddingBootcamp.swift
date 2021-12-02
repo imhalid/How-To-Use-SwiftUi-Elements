9011a291efc24e3bf5c8e8f57a56afc2552cf359
imhalid <imhalid@icloud.com>
2021-12-02 11:53:36 +0300

    deneme

---
+++ PaddingBootcamp.swift
@@ -0,0 +1,37 @@
+//
+//  PaddingBootcamp.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by Halit İçli on 1.12.2021.
+//
+
+import SwiftUI
+
+struct PaddingBootcamp: View {
+    var body: some View {
+        VStack {
+            Button {
+                print("d")
+            } label: {
+                VStack {
+                    Text("hello")
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 7)
+                        .background(Color.yellow)
+                        .cornerRadius(10)
+                        .foregroundColor(.black)
+                    
+                }.padding(.bottom, 3)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+                
+            }
+        }
+    }
+}
+
+struct PaddingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PaddingBootcamp()
+    }
+}
---
+++ ScrollViewBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViewBootcamp.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by Halid İçli on 2.12.2021.
+//
+
+import SwiftUI
+
+struct ScrollViewBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrollViewBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewBootcamp()
+    }
+}

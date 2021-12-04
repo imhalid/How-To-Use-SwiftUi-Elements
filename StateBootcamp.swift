0162f0e09743148ded4597c9da039f5638e59d79
imhalid <imhalid@icloud.com>
2021-12-04 14:13:45 +0300

    HTU State

---
+++ StateBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  StateBootcamp.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by Halid İçli on 4.12.2021.
+//
+
+import SwiftUI
+
+struct StateBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StateBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StateBootcamp()
+    }
+}

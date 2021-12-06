1829a5fb2086a522e1b41ff561685c7e430796d3
imhalid <imhalid@icloud.com>
2021-12-07 02:09:52 +0300

    Functions

---
+++ FunctionsBootcamp.swift
@@ -0,0 +1,54 @@
+//
+//  FunctionsBootcamp.swift
+//  SwiftUIThinkingBootcampApp
+//
+//  Created by Halid İçli on 7.12.2021.
+//
+
+import SwiftUI
+
+struct FunctionsBootcamp: View {
+    
+   @State var textBody: String = "Press Me"
+   @State var color: Color = Color.black
+
+    
+    var body: some View {
+        
+        VStack {
+            Text(textBody)
+                .font(.largeTitle)
+                .shadow(radius: 6)
+                
+            Button {
+                changeColorAndText()
+            } label: {
+                Text(textBody)
+                    .padding(5)
+                    .background(color)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    
+            }
+
+        }
+        
+    }
+    func changeColorAndText() {
+        color = Color.indigo
+        textBody = "Good job"
+    }
+}
+
+struct FunctionsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Rectangle().ignoresSafeArea()
+                .foregroundColor(.orange.opacity(0.2))
+            FunctionsBootcamp()
+            
+        }
+        
+            
+    }
+}
